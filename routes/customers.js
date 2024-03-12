baf2e088224efbef2b896260e53582490ca8adc1
shervinkayvon <shervinkayvon@gmail.com>
2024-03-11 21:44:04 -0700

    added auth support for customer routes

--- routes/customers.js
+++ routes/customers.js
@@ -2,12 +2,14 @@ const {Customer, validate} = require('../models/customer');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+const auth = require('../middleware/auth');
+
+router.get('/', auth, async (req, res) => {
   const customers = await Customer.find().sort('name');
   res.send(customers);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -24,7 +26,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -40,7 +42,7 @@ router.put('/:id', async (req, res) => {
   res.send(customer);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
   const customer = await Customer.findByIdAndRemove(req.params.id);
 
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
@@ -48,7 +50,7 @@ router.delete('/:id', async (req, res) => {
   res.send(customer);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
   const customer = await Customer.findById(req.params.id);
 
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
